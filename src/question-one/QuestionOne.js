b4e694e3cf4eb3e729bf7c5bf87e7ec9a5874910
Lucky <lucky@siliconstack.com.au>
2020-08-06 08:28:01 +0700

    Added: (Question 1) search function

--- src/question-one/QuestionOne.js
+++ src/question-one/QuestionOne.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { SectionGroup } from '../components/section/SectionGroup'
-import { SectionPanel } from '../components/section/SectionPanel'
+import { SectionGroup } from "../components/section/SectionGroup";
+import { SectionPanel } from "../components/section/SectionPanel";
 
-import './QuestionOne.css'
+import "./QuestionOne.css";
 
-export const QuestionOne = (props) => {
+export const QuestionOne = ({ service }) => {
+  const [data, setData] = useState([]);
+
+  const search = (keyword) => {
+    if (!keyword) {
+      setData([]);
+
+      return;
+    }
+
+    if (keyword.length >= 3) {
+      service.getJobsWithSearchTerm(keyword).then((res) => {
+        setData(res);
+      });
+    }
+  };
+
   return (
     <SectionGroup>
       <SectionPanel>
-        Please refer to INSTRUCTIONS.md
+        <form>
+          <input
+            onChange={(e) => search(e.target.value)}
+            type="text"
+            placeholder="Search..."
+          />
+        </form>
+        <table>
+          {data.map((item) => (
+            <tr>
+              <td>{item.name}</td>
+              <td>{item.start}</td>
+              <td>{item.end}</td>
+            </tr>
+          ))}
+        </table>
       </SectionPanel>
     </SectionGroup>
-  )
-}
+  );
+};
