ef2494b6c849d10a60208d4b7d00b64ea181a56f
Lucky <lucky@siliconstack.com.au>
2020-08-10 14:51:57 +0700

    Added: (Question 3) Layout

--- src/question-three/QuestionThree.js
+++ src/question-three/QuestionThree.js
@@ -1,16 +1,79 @@
 import React from 'react';
 
-import { SectionGroup } from '../components/section/SectionGroup'
-import { SectionPanel } from '../components/section/SectionPanel'
+import { SectionGroup } from '../components/section/SectionGroup';
+import { SectionPanel } from '../components/section/SectionPanel';
 
-import './QuestionThree.css'
+import './QuestionThree.scss';
+
+const Card = (props) => {
+  return <div className="card"></div>;
+};
+
+const Header = ({ headerItems }) => {
+  return (
+    <nav>
+      {headerItems.map((item) => (
+        <a href="#" key={item}>
+          {item}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+const SideBar = (props) => (
+  <div className="sidebar">
+    <div className="menu">
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+    </div>
+  </div>
+);
+
+const ColumnLeft = () => (
+  <div className="column-left">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+);
+
+const ColumnRight = () => (
+  <div className="column-right">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+);
 
 export const QuestionThree = (props) => {
+  const headerItems = ['Home', 'About', 'Jobs', 'Contact'];
+
   return (
     <SectionGroup>
       <SectionPanel>
-        Please refer to INSTRUCTIONS.md
+        <div className="wrapper">
+          <SideBar />
+          <Header headerItems={headerItems} />
+          <ColumnLeft />
+          <ColumnRight />
+        </div>
       </SectionPanel>
     </SectionGroup>
-  )
-}
+  );
+};
